cc358d582a6418ace45505bc8dfba25a8892fdc4
networksaphyra <networksaphyra004@gmail.com>
2024-11-22 23:51:34 -0500

    feat: i kinda forgot what i added to all of them ;).

--- js/App.js
+++ js/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Hero from "./Hero";
+import Content from "./Content";
 import styles from "../css/App.module.css";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <main>
       <Header />
       <Hero />
+      <Content />
     </main>
   );
 };
