d1717e4354d47c41e84e6c7fc4e10a0d4d826204
networksaphyra <networksaphyra004@gmail.com>
2024-12-06 02:00:33 -0500

    feat: this should've been in the last commit.

--- js/App.js
+++ js/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Hero from "./Hero";
 import Content from "./Content";
 import Team from "./Team";
+import Footer from "./Footer";
 import styles from "../css/App.module.css";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <Content />
       <Team />
+      <Footer />
     </main>
   );
 };
